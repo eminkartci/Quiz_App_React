c65298836b2a8f13f3b3fd6053cc186e9a392327
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-26 22:17:43 +0300

    Questions can be fetched - console Log

--- quiz_app_react/src/App.tsx
+++ quiz_app_react/src/App.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from 'react';
-
+import {fetchQuizQuestions} from './API';
 // Import Question Card Component
 import QuestionCard from './components/QuestionCard';
+
+// Question Types
+import {Difficulty} from './API'
 
 // CONSTANTS
 const TOTAL_QUESTIONS = 10;
@@ -16,6 +19,8 @@ const App = () => {
   const [userAnswers,setUserAnswers] = useState([]);
   const [score,setScore] = useState(0);
   const [gameOver,setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.MEDIUM))
 
   // Trivia Fetch Questions
   const startTrivia = async () =>{
@@ -42,7 +47,7 @@ const App = () => {
       <p className="score"> Doğru Sayısı:  </p>
       <p> Sorular Yükleniyor...  </p>
 
-      <QuestionCard
+      {/* <QuestionCard
 
         questionNr = {number + 1}
         totalQuestions = {TOTAL_QUESTIONS}
@@ -50,8 +55,8 @@ const App = () => {
         answers = {questions[number].answers}
         userAnswer = {userAnswers ? userAnswers[number] : undefined}
         callback = {checkAnswer}
-        
-      />
+
+      /> */}
 
       <button className="next" onClick={nextQuestion}> Sonraki Soru </button>
     </div>
